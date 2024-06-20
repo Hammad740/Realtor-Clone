0839852f16aea5d61c7030ec03e88f85591aa842
Hammad740 <mohammadhammadansari91@gmail.com>
2024-06-20 16:59:23 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-400">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
+        veniam aperiam in mollitia doloremque, ducimus deleniti ex
+        reprehenderit? Animi debitis expedita corporis molestiae quod, eos
+        libero voluptatibus aut voluptate quas?
+      </h1>
+    </div>
+  );
+};
+
+export default App;
