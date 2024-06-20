67f20e1e116d1649e1eb209df58fd2386df05372
Hammad740 <mohammadhammadansari91@gmail.com>
2024-06-20 17:44:01 +0530

    pages created and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Profile from './Pages/Profile';
+import Signin from './Pages/Signin';
+import Signup from './Pages/Signup';
+import Offers from './Pages/Offers';
+import ForgotPassword from './Pages/ForgotPassword';
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-red-400">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto
-        veniam aperiam in mollitia doloremque, ducimus deleniti ex
-        reprehenderit? Animi debitis expedita corporis molestiae quod, eos
-        libero voluptatibus aut voluptate quas?
-      </h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/sign-in" element={<Signin />}></Route>
+        <Route path="/sign-up" element={<Signup />}></Route>
+        <Route path="/offers" element={<Offers />}></Route>
+        <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+      </Routes>
+    </Router>
   );
 };
 
